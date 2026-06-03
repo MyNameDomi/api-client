545a57375f115f4f21ec52783996481a027a45a8
MyNameDomi <aigner_dominik@web.de>
2026-06-03 06:21:31 +0200

    feat: add argparse CLI for latitude and longitude input

--- weather.py
+++ weather.py
@@ -1,15 +1,36 @@
 import requests
 import logging
+import argparse
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s", force=True)
 
-URL = "https://api.open-meteo.com/v1/forecast?latitude=48.14&longitude=11.58&current_weather=true"
 
-def get_data () -> tuple[str, float, float] :
+
+default_lat =48.14
+default_lot =11.58
+
+
+def get_input() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lat", type=float, default=48.14, help="which latitude")
+    parser.add_argument("--lon", type=float, default=11.58, help="which Longitude")
+
+    user_input = parser.parse_args()
+
+    return user_input
+
+def build_url(user_input: argparse.Namespace) -> str:
+
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={user_input.lat}&longitude={user_input.lon}&current_weather=true"
+    
+    return url
+
+
+def get_data (url) -> tuple[str, float, float] :
 
     try:
 
-        req = requests.get(URL)
+        req = requests.get(url)
         req.raise_for_status()
         
 
@@ -32,7 +53,9 @@ def write_data(time:str, temperature:float, windspeed:float) -> None:
 
 
 if __name__ == "__main__":
-    result = get_data()
+    user_input = get_input()
+    url = build_url(user_input)
+    result = get_data(url)
     if result is None:
         logging.warning("No data received. Exiting.")
     else:
